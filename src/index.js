ee314b44b627515363cb0983b326cd114b9f7cae
peni123 <peni.dineva@gmail.com>
2017-07-29 14:48:17 +0300

    work in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-let msg = 'hello';
+import React from 'react';
+import {render} from 'react-dom';
+import App from 'components/app';
 
-console.log(msg);
+render(<App/>, document.getElementById('app'));
